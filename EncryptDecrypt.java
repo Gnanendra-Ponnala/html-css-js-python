518a136064bd9bf1da13986faebf309eb1a88f12
Gnanendra Reddy Ponnala <47503296+Gnanendra-Ponnala@users.noreply.github.com>
2021-07-12 14:50:59 +0530

    Create EncryptDecrypt.java

---
+++ EncryptDecrypt.java
@@ -0,0 +1,13 @@
+public class EncryptDecrypt{
+  public static void main(String... args){
+    String en = "gnanendra";
+    int key = 1;
+    char[] chars = en.toCharArray();
+    for(char c : chars){
+      c += key; 
+      System.out.print(c);
+    }
+  }
+  /*
+   make c-=key;
+}
